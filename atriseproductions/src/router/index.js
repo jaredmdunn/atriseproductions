7f64f4f67697c6db83e5b2d99f0e077615df3324
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-27 14:42:37 -0500

    Add views and routes

--- atriseproductions/src/router/index.js
+++ atriseproductions/src/router/index.js
@@ -14,6 +14,30 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Events.vue')
+  },
+  {
+    path: '/events/:id',
+    name: 'Event',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Event.vue')
+  },
+  {
+    path: '/get-involved',
+    name: 'GetInvolved',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/GetInvolved.vue')
   }
 ]
 
