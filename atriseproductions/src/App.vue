ceaa889513dc79de49d375011d7c2d2aa44645cc
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-27 15:48:18 -0500

    Set up Sass

--- atriseproductions/src/App.vue
+++ atriseproductions/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <TheNav />
   <router-view/>
+  <footer>
+    <a href="https://www.facebook.com/CNYAtRise/" class="transparent"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.instagram.com/cnyatrise/" class="transparent"><i class="fab fa-instagram"></i></a>
+    <p>&copy; 2020 At Rise Productions. All rights reserved.</p>
+  </footer>
 </template>
 
+<script>
+import TheNav from './components/TheNav.vue'
+export default {
+  components: { 
+    TheNav 
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import './assets/css/custom.css'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ atriseproductions/src/components/TheNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="bg-primary">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-link to="/get-involved">Get Involved</router-link>
+    <router-link to="/events">Events</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
