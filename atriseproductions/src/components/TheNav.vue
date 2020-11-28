076694e41b52c2769d01df49d67bd530f763f8cc
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-27 22:42:05 -0500

    Create initial nav

--- atriseproductions/src/components/TheNav.vue
+++ atriseproductions/src/components/TheNav.vue
@@ -1,9 +1,32 @@
 <template>
-  <nav class="bg-primary">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/get-involved">Get Involved</router-link>
-    <router-link to="/events">Events</router-link>
+  <nav class="navbar navbar-expand-sm navbar-light bg-orange sticky-top p-0">
+    <div class="container-fluid">
+      <button class="navbar-toggler m-auto border-0" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse container-fluid" id="main-nav">
+        <ul class="navbar-nav d-flex justify-content-evenly col text-center">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://mkt.com/atrise">Tickets</a>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/events">Events</router-link> 
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/get-involved">Get Involved</router-link> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://mkt.com/atrise2">Support</a>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link> 
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
