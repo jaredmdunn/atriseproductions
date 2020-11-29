0c16476b496c9998158ffe3a423ec88242599c08
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-28 22:28:11 -0500

    Remove events page

--- atriseproductions/src/router/index.js
+++ atriseproductions/src/router/index.js
@@ -16,15 +16,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/events',
-    name: 'Events',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Events.vue')
-  },
-  {
-    path: '/events/:id',
+    path: '/events/:slug',
     name: 'Event',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
