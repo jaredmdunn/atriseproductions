34634935e425c725e70a053acac7e4b926fe43c9
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-28 22:27:12 -0500

    Add events array data

---
+++ atriseproductions/src/assets/js/events.js
@@ -0,0 +1,46 @@
+const events = [
+  {
+    id: 1,
+    title: "You're a Good Man, Charlie Brown",
+    slug: "charlie-brown",
+    by: "Clark Gesner, Michael Mayer, and Andrew Lippa",
+    description: "<p><em>You're A Good Man, Charlie Brown</em> was <span class=\"text-blue\">At Rise Productions</span>' first spring production. Our team of seventeen young people from ages 12 to 18 made up the entire cast, crew, pit, and creative team. For this production, our Founding Artistic Director, Jared Dunn, received a SALT (Syracuse Area Live Theater) award nomination for Best Director of a Musical in community theater. It took place at Atonement Ministries with the support of Appleseed Community.</p>",
+    eventPhotoCredit: "Genevieve Fridley",
+    quote: "\"Congratulations to everyone at CNY At Rise Productions on a wonderful production of <em>You're A Good Man, Charlie Brown</em>! Looking forward to many more quality shows from this exciting new theatre company in Central New York. Welcome to our amazing theatre community!\"",
+    quoteBy: "Greg Lewis",
+    date: "May 2018",
+    program: true
+  },
+  {
+    id: 2,
+    title: "Summer Cabaret 2018",
+    slug: "summer-cabaret-2018",
+    description: "<p><span class=\"text-blue\">At Rise Productions</span>' 2018 summer cabaret began as a collaboration between Jared Dunn and Nancy O'Connor to write as many songs as they could and then get a group of talented teen singers to perform them.</p><p>They had just started rehearsals when their good friend, Blake Lucas, who had performed the role of Linus in <em>You're A Good Man, Charlie Brown</em>, suddenly and unexpectedly passed away. With the permission of Blake's family, the cabaret was turned into an event to honor Blake's memory and to raise money for his family. On August 21, after the funeral ceremony, eighteen students, including the entire cast of <em>You're A Good Man, Charlie Brown</em>, performed eleven original songs for an audience of over 300 people.</p>",
+    date: "August 2018",
+    program: false,
+    video: "https://www.youtube.com/watch?v=ECesrMWeitE&feature=youtu.be"
+  },
+  {
+    id: 3,
+    title: "Band Geeks",
+    slug: "band-geeks",
+    by: "Mark Allen, Gaby Alter, Tommy Newman, and Gordon Greenberg",
+    description: "<p><em>Band Geeks</em> was <span class=\"text-blue\">At Rise Productions</span>' second spring musical. We had nearly triple the number of young people working on <em>Band Geeks</em> as we had for the previous year's production of <em>You're A Good Man, Charlie Brown</em>, and we expanded to include people from seven CNY school districts. We also had adults in our cast for the first time. <em>Band Geeks</em> took place at the Catherine Cummings Theatre in Cazenovia.</p><p><em>Band Geeks</em> received <a href=\"https://familytimescny.com/2019/10/17/nominees-for-2019-salt-awards-revealed/\">5 SALT nominations</a> in the community theater category:</p><ul><li>Best Musical</li> <li>Best Director (Olivia DeAngelis)</li><li>Best Music Director (Nancy O'Connor)</li><li>Best Choreographer (Hannah Komar)</li> <li>Best Set Design (Maria and Oliva DeAngelis)</li></ul>",
+    eventPhotoCredit: "Genevieve Fridley",
+    date: "May 2019",
+    program: true
+  },
+  {
+    id: 4,
+    title: "Teenage Nobodies",
+    slug: "teenage-nobodies",
+    by: "O'Connor & Dunn",
+    description: "<p><em>Teenage Nobodies</em> follows the stories of several modern-day teens as they encounter some of the first major decisions and changes in their lives.</p><p>We raised funds for the Blake Lucas Memorial Music/Theater Scholarship Fund in honor of our dear friend Blake who died August 16, 2018. With the help of the <a href=\"https://cnycf.org/extraordinary#.XhjA78hKhPY\">Carrie Lazarus Fund for Extraordinary Talent</a>, this scholarship will go to a young person pursuing their love for music and/or theater every year.</p>",
+    eventPhotoCredit: "Genevieve Fridley",
+    date: "August 2019",
+    program: true,
+    video: "https://www.youtube.com/playlist?list=PLHdDvXi2B7t3-dUCHo94DmJL30a1DuOJ3"
+  },
+];
+
+export default events;
