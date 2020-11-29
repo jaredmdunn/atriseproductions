00f2db5daffad330a8a13425f520f22b140be8e9
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-28 22:28:41 -0500

    Create basic home layout

--- atriseproductions/src/views/Home.vue
+++ atriseproductions/src/views/Home.vue
@@ -1,59 +1,21 @@
 <template>
-  <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem ducimus, totam quisquam omnis et temporibus incidunt sint sapiente amet cumque sequi, vel nam delectus quis culpa quae dolorum soluta.</p>
-    Hi
-  </div>
+  <main class="container-fluid p-0">
+    <section>
+      <img src="@/assets/images/cover-photo.jpg" alt="In Front of Catherine Cummings Theater" class="img-fluid">
+    </section>
+    <aside class="bg-orange text-center row mx-4 justify-content-center">
+      <div class="col col-md-9 col-lg-6">
+        <h2 class="text-blue">At Rise Productions</h2>
+        <p>is a Central New York based theater company dedicated to providing 
+          unique opportunities for young people to participate in all parts 
+          of putting on a theatrical production.</p>
+        <p><router-link to="/about">Read our story!</router-link></p>
+      </div>
+    </aside>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
