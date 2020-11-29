999ba09f6dcf63976ecf3785b995836065a508b4
jaredmdunn <67663613+jaredmdunn@users.noreply.github.com>
2020-11-28 22:28:25 -0500

    Create event view

---
+++ atriseproductions/src/views/Event.vue
@@ -0,0 +1,23 @@
+<template>
+  <EventTitleCard :event="event" />
+</template>
+
+<script>
+import EventTitleCard from '../components/EventTitleCard.vue';
+import events from '../assets/js/events';
+
+export default {
+  components: { 
+    EventTitleCard 
+  },
+  computed: {
+    event() {
+      return events.find((event) => event.slug === this.$route.params.slug);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
