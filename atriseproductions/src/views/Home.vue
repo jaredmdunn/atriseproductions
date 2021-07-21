5226484e193b73f801b29a71d381ed2160999df7
Jared Dunn <jdunn@webucator.com>
2021-07-21 06:24:00 -0400

    Fix links

--- atriseproductions/src/views/Home.vue
+++ atriseproductions/src/views/Home.vue
@@ -4,7 +4,7 @@
   </section>
   <section class="mx-4">
     <h1 class="text-blue">Upcoming Events!</h1>
-      <p><strong>Fri. July 23, 2021 at 6:00 PM</strong> - <em><strong>A CABARET FOR THE ENVIRONMENT</strong></em>: <span class="text-blue">At Rise</span>, <a href="https://https://movementsthroughmu.wixsite.com/elenaandkira/director-s-note">Movements Through Music</a>, and <a href="https://www.facebook.com/sustainable.manlius/">Sustainable Manlius</a> are organizing an evening of live music and environmental change at the <a href="https://https://fflib.org/">Fayetteville Free Library</a> in the music pavilion. Bring a lawn chair and join us for a great night of performances, learn about the Town of Manlius community campaign, <em>Get Sustainable, Manlius!</em> and the local sustainability efforts being taken by local organizations, such as <a href="https://heatsmartcny.org/">HeatSmart CNY</a>. Click <a href="https://www.eventbrite.com/e/a-cabaret-for-the-environment-tickets-162821903697?fbclid=IwAR0BOxZNiPV4R1Ys5amazzMx0Bwfm2SNUTd_EEviwTgvh8T_Rwc1JtaogJw">here</a> to register for this free event.</p>
+      <p><strong>Fri. July 23, 2021 at 6:00 PM</strong> - <em><strong>A CABARET FOR THE ENVIRONMENT</strong></em>: <span class="text-blue">At Rise</span>, <a href="https://movementsthroughmu.wixsite.com/elenaandkira/director-s-note">Movements Through Music</a>, and <a href="https://www.facebook.com/sustainable.manlius/">Sustainable Manlius</a> are organizing an evening of live music and environmental change at the <a href="https://fflib.org/">Fayetteville Free Library</a> in the music pavilion. Bring a lawn chair and join us for a great night of performances, learn about the Town of Manlius community campaign, <em>Get Sustainable, Manlius!</em> and the local sustainability efforts being taken by local organizations, such as <a href="https://heatsmartcny.org/">HeatSmart CNY</a>. Click <a href="https://www.eventbrite.com/e/a-cabaret-for-the-environment-tickets-162821903697?fbclid=IwAR0BOxZNiPV4R1Ys5amazzMx0Bwfm2SNUTd_EEviwTgvh8T_Rwc1JtaogJw">here</a> to register for this free event.</p>
       <p><strong>Thurs. July 29, 2021 at 7:00 PM</strong> - <em><strong>LET'S GO TO THE MOVIES</strong></em>: <span class="text-blue">At Rise</span> will be participating in <a href="http://www.frontrowplayers.org">Front Row Players’</a> original production, <em>Let’s Go to the Movies</em> at <a href="https://www.fmschools.org/fayetteville-manlius-high-school/">Fayetteville-Manlius High School</a>. Click <a href="http://www.frontrowplayers.org/upcoming/">here</a> to register for your free ticket today!</p>
   </section>
   <aside class="bg-orange text-center row mx-4 justify-content-center">
@@ -20,12 +20,12 @@
 </template>
 
 <script>
-import PastProductions from '../components/PastProductions.vue'
+import PastProductions from "../components/PastProductions.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    PastProductions
-  }
-}
+    PastProductions,
+  },
+};
 </script>
